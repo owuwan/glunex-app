ad1b3f0e73bbad315f9b6b47da176735209d8855
owuwan <chol5622729@naver.com>
2026-01-27 14:01:07 +0900

    Vercel 라우팅 설정 강제 적용

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // [중요] 빌드 시 경로 문제 해결을 위한 설정
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  }
 })
