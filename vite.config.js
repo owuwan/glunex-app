50c1df0664a2760deec3dc4cd5a5803f9dbf9b2e
owuwan <chol5622729@naver.com>
2026-01-27 14:24:29 +0900

    배포 설정 강제 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// 복잡한 경로 설정 다 빼고 기본으로 갑니다.
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist', // 결과물은 무조건 dist 폴더로
+  }
 })
